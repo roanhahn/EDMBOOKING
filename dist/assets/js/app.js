47e06276676d17dd02dbab9fd0e0b6b180dbaa57
Roan Hahn <roanhahn@gmail.com>
2018-03-08 16:10:31 +0100

    first commit

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -203,3 +203,37 @@ jQuery.extend( jQuery.easing,
  * OF THE POSSIBILITY OF SUCH DAMAGE. 
  *
  */
+
+
+// var bodyheight = $('.sitewrapper').outerHeight() - $('.header').outerHeight();
+
+// $('.subnav').css("height", bodyheight);
+
+// console.log(bodyheight);
+
+// SHOW HAMBURGER MENU
+
+$('.toggle-subnav').click(function(){
+	 $('#subnav').toggleClass("subnav-active");
+	 $('.body, .html').toggleClass("scroll-lock");
+	 // $('body, .html').attr('scroll','no');
+ });
+
+// OWL
+
+$(document).ready(function() {
+	var owlReviews = $('.owl-reviews');
+	owlReviews.owlCarousel({
+		items:1,
+		autoplay:true,
+		autoplayTimeout:3000,
+		autoplayHoverPause:true,
+		loop: true,
+		autoplaySpeed: 1000,
+		navSpeed: 1000,
+		dotsSpeed: 1000,
+		dragEndSpeed: 1000
+		// animateOut: 'slideOutDown',
+		// animateIn: 'flipInX',
+	})
+});
