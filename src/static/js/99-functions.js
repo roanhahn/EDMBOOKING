f7e8ae975b7cdda9e0b0125bc320b914eaca54af
Roan Hahn <roanhahn@gmail.com>
2018-03-14 12:09:20 +0100

    wednesday

--- src/static/js/99-functions.js
+++ src/static/js/99-functions.js
@@ -15,7 +15,8 @@ $("img.lazyload").lazyload();
 $('.toggle-subnav').click(function(){
 	 $('.hamburger').toggleClass("hamburger-close");
 	 $('#subnav').toggleClass("subnav-active");
-	 $('.body, .html').toggleClass("scroll-lock");
+	 // $('.body, .html').toggleClass("scroll-lock");
+	 return false;
 	 // $('body, .html').attr('scroll','no');
  });
 
@@ -27,11 +28,18 @@ $('.readmore').click(function(){
 	$(this).closest('.room-content').find('.room-info').toggleClass("room-info--active");
 	 // $('body, .html').attr('scroll','no');
  });
+
+
+// NOTIFICATION
+$('.notification').delay(500).fadeOut("fast");
+
+
 // SHOW YOUR BOOKING
 
 $('.toggle-yourbooking').click(function(){
 	 $('.yourbooking').slideToggle(300);
-	 $('.body, .html').toggleClass("scroll-lock");
+	 return false;
+	 // $('.body, .html').toggleClass("scroll-lock");
 	 // $('body, .html').attr('scroll','no');
  });
 
@@ -46,6 +54,26 @@ function goBack() {
 
 $(function() {
 	$('select').selectric();
+});
+
+// SCROLL HEADER
+
+var headerheight = $(".header").outerHeight();
+
+console.log(headerheight);
+
+$(window).scroll(function() {    
+	var scroll = $(window).scrollTop();
+	
+
+	if (scroll >= headerheight) {
+		$(".body").addClass("scrolled");
+		// $(".site-background").addClass("scrolled");
+	} else {
+		$(".body").removeClass("scrolled");
+		// $(".site-background").removeClass("scrolled");
+	}
+	
 });
 
 // OWL
